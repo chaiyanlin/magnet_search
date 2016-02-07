a0aba9fc89d85963db4027df73aa1d9c206e928c
Yanlin Chai <chai.yanlin@lingdong365.com>
2016-02-07 13:47:54 +0800

    优化输出格式

--- fetch.php
+++ fetch.php
@@ -26,7 +26,8 @@ function decode_uri_component($str) {
     preg_match('/decodeURIComponent\((.+?)\)/i', $str, $matches, PREG_OFFSET_CAPTURE);
     $str_decoded = preg_replace('/\+/', '', $matches[1][0]);
     $str_decoded = preg_replace('/"/', '', $str_decoded);
-    $str_decoded = iconv('UTF-8', 'UTF-8', urldecode($str_decoded));
+    #$str_decoded = iconv('UTF-8', 'UTF-8', urldecode($str_decoded));
+    $str_decoded = urldecode($str_decoded);
     return $str_decoded;
 }
 
@@ -43,11 +44,10 @@ function get_parameter($argv) {
 
 function trace( $input ) {
 	if ( is_string($input) ) {
-		$str = date("Y-m-d H:i:s") . ": $input".PHP_EOL;
+		$str = $input.PHP_EOL;
 	}
 	else {
-		$str = date("Y-m-d H:i:s") . ':' . PHP_EOL;
-		$str .= print_r($input, true);
+		$str = print_r($input, true);
 	}
 	echo $str;
 }
@@ -57,8 +57,9 @@ function trace( $input ) {
 //执行开始
 $codes = get_parameter($argv);
 $url = 'http://www.btlibrary.net';
+trace('查询开始...');
 foreach ( $codes as $code ) {
-	trace('正在搜索关键字：' . $code);
+	trace('正在搜索关键字：' . $code . ' >>>>>');
 	$data = array('s'=> $code);
 
 	$snoopy = new Snoopy();
@@ -68,14 +69,18 @@ foreach ( $codes as $code ) {
 
 	$html = str_get_html($result);
 
-	$list = array();
-	foreach ($html->find('div[class=item]') as $item) {
+	$items = $html->find('div[class=item]');	
+	trace('结果返回' . count($items) . '条:');
+	foreach ($items as $item) {
 	    $title   = get_title($item);
 	    $size    = get_size($item);
 	    $magnent = get_magnent_url($item);
-	    $list[] = compact('title', 'size', 'magnent');
+  	    trace('{');
+	    trace('title: ' . $title);
+	    trace('size: ' . $size);
+	    trace('magnent: ' . $magnent);
+            trace('}' . PHP_EOL );
 	}
-
-	print_r($list);
+	trace('<<<<<' . PHP_EOL . PHP_EOL . PHP_EOL);
 }
-
+trace('查询结束...');
